ae9d1df74840b209d3734845578645c9741305a5
J Cole Morrison <me@jcolemorrison.com>
2024-03-19 10:51:03 -0700

    update backend points for https

--- src/backend.ts
+++ src/backend.ts
@@ -1,7 +1,9 @@
 export const SERVER_PORT = process.env.SERVER_PORT || 2567;
 
-export const BACKEND_URL = (window.location.href.indexOf("localhost") === -1)
-    ? `${window.location.protocol.replace("http", "ws")}//${process.env.SERVER_HOST || window.location.hostname}:${SERVER_PORT}`
-    : `ws://localhost:${SERVER_PORT}`;
+export const BACKEND_URL = process.env.APP_ENV === "production"
+    ? `${window.location.protocol.replace("https", "wss")}//${process.env.SERVER_HOST || window.location.hostname}:${SERVER_PORT}`
+    : `${window.location.protocol.replace("http", "ws")}//${process.env.SERVER_HOST || window.location.hostname}:${SERVER_PORT}`
 
-export const BACKEND_HTTP_URL = BACKEND_URL.replace("ws", "http");
+export const BACKEND_HTTP_URL = process.env.APP_ENV === "production"
+    ? BACKEND_URL.replace("wss", "https")
+    : BACKEND_URL.replace("ws", "http")
